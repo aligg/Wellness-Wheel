75a49863f54dfe4a07163eedeb02324c79fc3974
naveenkumar <naveenkumar.jp@zohocorp.com>
2019-10-05 15:19:31 +0530

    calm text added right aligned in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,19 @@ import * as theme from '../../public/static/theme';
 const Header = ({
   toggleDrawer,
 }) => (
-  <div>
+  <div style={{display : 'flex'}}>
     <MenuIcon
       color={theme.colors.grey}
       onClick={toggleDrawer}
       size={30}
       style={{ cursor: 'pointer' }}
     />
+    <p style={{
+      color: theme.colors.grey,
+      fontSize: 12,
+      position : 'relative',
+      left : '96%'
+    }}>calm</p>
   </div>
 );
 
